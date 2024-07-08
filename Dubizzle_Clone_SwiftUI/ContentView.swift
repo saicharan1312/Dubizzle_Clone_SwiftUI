b51ddc2a99088ccc47baf7c1b1c825ea5dab8c5f
Sai Charan <saicharan12nov@gmail.com>
2024-07-08 11:44:01 -0400

    Initial Commit

---
+++ Dubizzle_Clone_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dubizzle_Clone_SwiftUI
+//
+//  Created by Sai Charan Thummalapudi on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dubizzle_Clone_SwiftUI/Dubizzle_Clone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dubizzle_Clone_SwiftUIApp.swift
+//  Dubizzle_Clone_SwiftUI
+//
+//  Created by Sai Charan Thummalapudi on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Dubizzle_Clone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
